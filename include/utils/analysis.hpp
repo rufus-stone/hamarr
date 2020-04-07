f9034df62219abd9c9734462b7e43ad86f9b4a48
rufus-stone <60827786+rufus-stone@users.noreply.github.com>
2020-04-07 15:41:40 +0100

    Added case insensitivity option for character frequency analysis

--- include/utils/analysis.hpp
+++ include/utils/analysis.hpp
@@ -8,6 +8,8 @@
 namespace analysis
 {
 
+enum class case_sensitivity { enabled, disabled };
+
 ////////////////////////////////////////////////////////////
 std::size_t hamming_distance(const std::string &lhs, const std::string &rhs)
 {
@@ -42,14 +44,34 @@ std::size_t hamming_distance(const std::string &lhs, const std::string &rhs)
 
 
 ////////////////////////////////////////////////////////////
-std::vector<std::size_t> character_frequency(const std::string &input)
+std::vector<std::size_t> character_frequency(const std::string &input, analysis::case_sensitivity sensitivity = analysis::case_sensitivity::enabled)
 {
   auto freqs = std::vector<std::size_t>(std::numeric_limits<uint8_t>::max(), 0); // Create a vector of 256 entries (one for each possible byte value), and set all to 0
 
-  // Increment the value at the index of the current char by one
-  for (const auto &ch : input)
+  switch (sensitivity)
   {
-    freqs[static_cast<uint8_t>(ch)] += 1;
+    case analysis::case_sensitivity::enabled:
+    {
+      // Increment the value at the index of the current char by one
+      for (const auto &ch : input)
+      {
+        freqs[static_cast<uint8_t>(ch)] += 1;
+      }
+    }
+    break;
+
+    case analysis::case_sensitivity::disabled:
+    {
+      // Normalise to all lowercase
+      auto tmp = format::to_lower(input);
+
+      // Increment the value at the index of the current char by one
+      for (const auto &ch : tmp)
+      {
+        freqs[static_cast<uint8_t>(ch)] += 1;
+      }
+    }
+    break;
   }
 
   return freqs;
@@ -57,7 +79,7 @@ std::vector<std::size_t> character_frequency(const std::string &input)
 
 
 ////////////////////////////////////////////////////////////
-void print_character_frequency(std::vector<uint8_t> freqs, bool show_zeros = false)
+void print_character_frequency(std::vector<std::size_t> freqs, bool show_zeros = false)
 {
   for (std::size_t i = 0; i < freqs.size(); ++i)
   {
