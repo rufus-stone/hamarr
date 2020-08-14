5544652e82c8ae0158e13630a80413f1aaa963a6
rufus-stone <60827786+rufus-stone@users.noreply.github.com>
2020-08-14 08:58:32 +0100

    Started writing a proper CMakeLists.txt to enable installation of the library, and started separating the tests

---
+++ test/tests.cpp
@@ -0,0 +1,16 @@
+#define CATCH_CONFIG_MAIN // This tells the Catch2 header to generate a main
+
+#include "catch.hpp"
+
+unsigned int Factorial(unsigned int number)
+{
+  return number <= 1 ? number : Factorial(number - 1) * number;
+}
+
+TEST_CASE("Factorials are computed", "[factorial]")
+{
+  REQUIRE(Factorial(1) == 1);
+  REQUIRE(Factorial(2) == 2);
+  REQUIRE(Factorial(3) == 6);
+  REQUIRE(Factorial(10) == 3628800);
+}
