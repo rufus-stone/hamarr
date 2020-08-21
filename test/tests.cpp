654c5e4d2f7db62cdff5c4d91cc569292b258acc
rufus-stone <60827786+rufus-stone@users.noreply.github.com>
2020-08-21 09:37:34 +0100

    Started to separate out example usage from unit tests. Using Catch2 for tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,16 +1,40 @@
 #define CATCH_CONFIG_MAIN // This tells the Catch2 header to generate a main
 
+#include <string>
+
+#include <hamarr/hamarr.hpp>
+
 #include "catch.hpp"
 
-unsigned int Factorial(unsigned int number)
-{
-  return number <= 1 ? number : Factorial(number - 1) * number;
-}
 
-TEST_CASE("Factorials are computed", "[factorial]")
+using namespace std::string_literals;
+
+SCENARIO("Strings can be converted between uppercase and lowercase")
 {
-  REQUIRE(Factorial(1) == 1);
-  REQUIRE(Factorial(2) == 2);
-  REQUIRE(Factorial(3) == 6);
-  REQUIRE(Factorial(10) == 3628800);
+  GIVEN("A std::string with some data")
+  {
+    const auto test_data = std::string{"Hello, World!"};
+
+    REQUIRE(!test_data.empty());
+
+    WHEN("hmr::format::to_upper() is applied")
+    {
+      auto upper = hmr::format::to_upper(test_data);
+
+      THEN("the string is converted to all uppercase.")
+      {
+        REQUIRE(upper == "HELLO, WORLD!"s);
+
+        WHEN("hmr::format::to_upper() is applied")
+        {
+          auto lower = hmr::format::to_lower(test_data);
+
+          THEN("the string is converted to all lowercase.")
+          {
+            REQUIRE(lower == "hello, world!"s);
+          }
+        }
+      }
+    }
+  }
 }
